b9b6cee33047235b3d74a5296f930f9a5901e951
Jonathan <jonacom@lissismore.com>
2016-04-09 22:03:55 -0600

    playlists, iter 4

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   resources :artists do
-    resources :songs
+    resources :songs, only: [:new, :create]
   end
 
-  resources :songs
+  resources :songs, only: [:show]
+  resources :playlists, only: [:index, :new, :create, :show]
 end
